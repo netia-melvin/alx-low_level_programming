3b7e8fd1f5187f29ba8d86719962d634417c48f6
netia-melvin <melvinatemba18@gmail.com>
2023-03-31 14:05:22 -0700

    0

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -2,6 +2,10 @@
 #define LISTS_H
 
 #include <stdlib.h>
+/**
+ * Description: singly linked list node structure
+**/
+
 typedef struct list_s
 {
 	char *str;
@@ -15,4 +19,4 @@ list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
 
-#endif
+#endif /* LISTS_H */
