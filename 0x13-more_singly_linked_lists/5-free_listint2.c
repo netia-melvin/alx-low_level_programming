9c3bdb46926a5f3036f4c9da16cc360f7ab90585
netia-melvin <melvinatemba18@gmail.com>
2023-04-03 19:41:37 -0700

    5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -7,17 +7,17 @@
  * @d: Sets the head to NULL.
  */
 void free_listint2(listint_t **head)
-{      
-	listint_t *h;
+{
+	listint_t *mel;
 
 	if (head == NULL)
 		return;
 
 	while (*head)
 	{
-		h = (*head)->next;
+		mel = (*head)->next;
 		free(*head);
-		*head = h;
+		*head = mel;
 	}
 
 	head = NULL;
