063ed12bdae72154dada72b71a9cfc316e730ebe
netia-melvin <melvinatemba18@gmail.com>
2023-04-08 07:38:29 -0700

    0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -10,17 +10,22 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-  unsigned int val = 0;
-  int i = 0;
+ unsigned int k = 1;
+ unsigned int i=0;
+ int c;
+ unsigned int len;
 
-  if (b == NULL)
-    return 0;
+ len = strlen(b);
 
-  while (b[i] == '0' || b[i] == '1')
-  {  // Found another digit.
-    val <<= 1;
-    val += b[i]-'0';
-    i++;
-  }
-  return val;
+  for (c = len-1; c >= 0; c--)
+ {
+  if (b[c] != '0' && b[c] != '1')
+      return (0);
+    if (b[c] == '1')
+    {
+     i += k;
+     }
+    k *= 2;
+}
+ return (i);
 }
