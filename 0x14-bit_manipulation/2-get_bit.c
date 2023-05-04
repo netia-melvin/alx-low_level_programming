4c4ea9378d0e562c0b4859e153e4a6b74c816193
netia-melvin <melvinatemba18@gmail.com>
2023-05-04 16:11:45 -0700

    2

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -6,14 +6,13 @@
  *@index: position
  *Return: bit at position n
  */
-
 int get_bit(unsigned long int n, unsigned int index)
 {
-	 if (index >= sizeof(unsigned long int) * 8) 
-	 {
-	 return -1;
- 	 }
+	unsigned long int mask;
 
- 	 unsigned long int mask = 1UL << index;
- 	 return (n & mask) ? 1 : 0;
+	if (index >= sizeof(unsigned long int) * 6)
+		return (-1);
+
+	mask = 1UL << index;
+	return ((n & mask) ? 1 : 0);
 }
