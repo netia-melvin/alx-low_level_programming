7221b5e323cadec597cc207787e4a0f789bbbc75
netia-melvin <melvinatemba18@gmail.com>
2023-05-04 10:07:39 -0700

    0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -5,21 +5,21 @@
  * @b: pointer to a string of 0 to 1 characters
  * Return: unsined int
 */
+
 unsigned int binary_to_uint(const char *b)
 {
-    unsigned int result = 0;
-    int i = 0;
+	unsigned int answer = 0;
+	int i = 0;
 
-    if (b == NULL) {
-        return 0;
-    }
+	if (b == NULL)
+		return (0);
 
-    while (size_t i = 0; i < strlen(b); i++) {
-        if (b[i] != '0' && b[i] != '1') {
-            return 0;
-        }
-        result = (result << 1) + (b[i] - '0');
-    }
-
-    return result;
+	for (b[i] != '\0')
+	{
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
+		answer = (answer << 1) + (b[i] - '0');
+		i++;
+	}
+	return (answer);
 }
