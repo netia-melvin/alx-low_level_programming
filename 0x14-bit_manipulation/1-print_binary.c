b2a8d3c70cdd941924c8becb8ce6309b4d9d37ff
netia-melvin <melvinatemba18@gmail.com>
2023-05-04 10:15:25 -0700

    1

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_binary - convert number to binary
+ * @n: test parameter
+ */
+
+
+void print_binary(unsigned long int n)
+{
+	if (n > 1)
+	{
+	print_binary(n >> 1);
+	}
+
+	_putchar((n & 1) ? '1' : '0');
+}
